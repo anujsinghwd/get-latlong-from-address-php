85723ac22ddd84cf3ae35212c529b6c8916d62ec
anujsinghwd <anujsingh179@yahoo.com>
2017-01-16 23:58:38 +0530

    first commit

---
+++ getlatlng.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* Author: Anuj Singh
+* Function Name: getLatLong()
+* $address => Full address.
+* Return => Latitude and longitude of the given address.
+**/
+function getLatLong($address){
+    if(!empty($address)){
+        //Formatted address
+        $formattedAddr = str_replace(' ','+',$address);
+        //Send request and receive json data by address
+        $geocodeFromAddr = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.$formattedAddr.'&sensor=false'); 
+        $output = json_decode($geocodeFromAddr);
+        //Get latitude and longitute from json data
+        $data['latitude']  = $output->results[0]->geometry->location->lat; 
+        $data['longitude'] = $output->results[0]->geometry->location->lng;
+        //Return latitude and longitude of the given address
+        if(!empty($data)){
+            return $data;
+        }else{
+            return false;
+        }
+    }else{
+        return false;   
+    }
+}
+
+/**
+ * Use getLatLong() function like the following.
+ */
+$address = 'Minister Road, Secundarabad ,Hyderabad,Andhra Pradesh';
+$latLong = getLatLong($address);
+echo $latitude = $latLong['latitude']?$latLong['latitude']:'Not found';
+echo $longitude = $latLong['longitude']?$latLong['longitude']:'Not found';
+?>
